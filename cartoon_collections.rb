e977c15bdc5864dbc8422dd0af193e344f1f1d21
Johndhee Ballesteros <johndheeballesteros13@gmail.com>
2021-01-04 07:44:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,9 @@
 def greet_characters(array)
-  # Use `each` to enumerate over the provided array
-  #
-  # Print a custom greeting for each element
+
+  array.each do |char|
+    puts "Hello #{char}!"
 end
 
 def list_dwarves(array)
-  # Use `each_with_index` to enumerate over the provided array
-  #
-  # Print a numbered list of each element
+
 end
